58a337eb791184c4e2ac48e792e69f8f7f47c163
Euheny Bondarovich <euheny@mediahub.com>
2016-04-26 13:43:09 +0300

    fix ie

--- dist/lazy-lo.js
+++ dist/lazy-lo.js
@@ -1,3 +1,32 @@
+!window.addEventListener && (function (WindowPrototype, DocumentPrototype, ElementPrototype, addEventListener, removeEventListener, dispatchEvent, registry) {
+	WindowPrototype[addEventListener] = DocumentPrototype[addEventListener] = ElementPrototype[addEventListener] = function (type, listener) {
+		var target = this;
+
+		registry.unshift([target, type, listener, function (event) {
+			event.currentTarget = target;
+			event.preventDefault = function () { event.returnValue = false };
+			event.stopPropagation = function () { event.cancelBubble = true };
+			event.target = event.srcElement || target;
+
+			listener.call(target, event);
+		}]);
+
+		this.attachEvent("on" + type, registry[0][3]);
+	};
+
+	WindowPrototype[removeEventListener] = DocumentPrototype[removeEventListener] = ElementPrototype[removeEventListener] = function (type, listener) {
+		for (var index = 0, register; register = registry[index]; ++index) {
+			if (register[0] == this && register[1] == type && register[2] == listener) {
+				return this.detachEvent("on" + type, registry.splice(index, 1)[0][3]);
+			}
+		}
+	};
+
+	WindowPrototype[dispatchEvent] = DocumentPrototype[dispatchEvent] = ElementPrototype[dispatchEvent] = function (eventObject) {
+		return this.fireEvent("on" + eventObject.type, eventObject);
+	};
+})(Window.prototype, HTMLDocument.prototype, Element.prototype, "addEventListener", "removeEventListener", "dispatchEvent", []);
+
 (function() {
   var lazylo = {};
 
@@ -65,9 +94,15 @@
     lazylo.lazylo_list_maker();
   }, 50);
 
-  window.addEventListener("load", function() {
+  function lazylo_load() {
     clearInterval(lazylo.intervalObject);
     lazylo.lazylo_list_maker();
     lazylo.reveal();
+  }
+
+  window.addEventListener("load", function() {
+    lazylo_load();
   }, false);
+
+
 })()
